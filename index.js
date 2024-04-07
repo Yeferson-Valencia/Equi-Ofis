635a7df50e766b8bede8acb99ca3694a10387c85
Yeferson-Valencia <pepemanpro.17@gmail.com>
2024-04-07 04:27:39 +0000

    Falta arreglar como se visualizan las imagenes

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ class Producto {
         };
     }
 
+    getNombre(){
+        return this.nombre;
+    }
+
+    getClase(){
+        return this.clase;
+    }
+
+    getSubclase(){
+        return this.subclase;
+    }
+
+    getImagen(){
+        return this.imagen;
+    }
+
     // Método para actualizar los atributos del producto
     setProducto(nombre, clase, subclase, referenciaExterna, ruta, cantidad) {
         this.nombre = nombre;
@@ -87,24 +103,17 @@ class CarritoDeCompras {
             return 0;
         }
         return this.productos.reduce((total, producto) => total + producto.cantidad, 0);
-    }
+     }
 }
+
 
 class Tienda {
     constructor() {
         this.productos = [];
         this.carritoDeCompras = new CarritoDeCompras();
-        ;
     }
 
-    cargarProductosSiNecesario(url) {
-        if (this.carritoDeCompras.getProductos().length === 0) {
-            return this.cargarProductosDesdeCSV(url);
-        } else {
-            return Promise.resolve();
-        }
-    }
-
+    // Método para cargar productos desde un archivo CSV
     cargarProductosDesdeCSV(url) {
         return fetch(url)
             .then(response => {
@@ -125,81 +134,190 @@ class Tienda {
             });
     }
 
+    // Método para obtener todos los productos
     getProductos() {
         return this.productos;
     }
 
+    // Método para obtener un producto por su nombre
     getProductoPorNombre(nombre) {
         return this.productos.find(producto => producto.nombre === nombre);
     }
 
+    // Método para renderizar los productos en el carrito y actualizar el contador
     renderizarProductosEnCarritoYContador() {
-        const carProducts = document.querySelector('.car-products');
         const carritoIcon = document.getElementById('carrito-icon');
-        const contadorCarrito = document.getElementById('cart-counter');
-
-        contadorCarrito.textContent = this.carritoDeCompras.getCantidadTotal();
-
-        if (this.carritoDeCompras.getProductos().length > 0) {
-            carProducts.innerHTML = '';
-            this.carritoDeCompras.getProductos().forEach(producto => {
-                const productElement = document.createElement('div');
-                productElement.classList.add('car-product');
-                productElement.innerHTML = `
-                    <div style="display: flex; align-items: center;">
-                        <div class="product-image" style="flex: 1;">
-                            <img src="assets/Imagenes/${producto.imagen}" alt="${producto.nombre}" style="width: 50px; height: 50px; max-width: 100%; max-height: 100%;">
-                        </div>
-                        <div class="product-name" style="flex: 2;">
-                            <p>${producto.nombre}</p>
-                        </div>
-                        <div class="product-quantity" style="flex: 1; display: flex; align-items: center; justify-content: space-between;">
-                            <button class="btn btn-sm btn-secondary decrease-quantity">-</button>
-                            <span>${producto.cantidad}</span>
-                            <button class="btn btn-sm btn-primary increase-quantity">+</button>
-                        </div>
-                    </div>
-                `;
-                carProducts.appendChild(productElement);
-
-                const decreaseBtn = productElement.querySelector('.decrease-quantity');
-                const increaseBtn = productElement.querySelector('.increase-quantity');
-                const quantitySpan = productElement.querySelector('.product-quantity span');
-
-                decreaseBtn.addEventListener('click', () => {
-                    this.carritoDeCompras.quitarProducto(producto);
-                    this.renderizarProductosEnCarritoYContador();
-                });
-
-                increaseBtn.addEventListener('click', () => {
-                    producto.cantidad++;
-                    quantitySpan.textContent = producto.cantidad;
-                    contadorCarrito.textContent = this.carritoDeCompras.getCantidadTotal();
-                });
-            });
-        } else {
-            carProducts.innerHTML = '<p class="text-center">No hay artículos</p>';
-        }
+        const carritoCounter = document.getElementById('cart-counter');
+        const carProducts = document.querySelector('.car-products');
 
         carritoIcon.addEventListener('click', event => {
             event.preventDefault();
             carProducts.classList.toggle('active');
         });
+
+        const renderizarProductosEnCarrito = () => {
+            const carProductsList = document.querySelector('.car-products');
+            if (this.carritoDeCompras.getProductos().length > 0) {
+                carProductsList.innerHTML = '';
+                this.carritoDeCompras.getProductos().forEach(producto => {
+                    const productElement = document.createElement('div');
+                    productElement.classList.add('car-product');
+                    productElement.innerHTML = `
+                        <div style="display: flex; align-items: center;">
+                            <div class="product-image" style="flex: 1;">
+                                <img src="assets/Imagenes/${producto.imagen}" alt="${producto.nombre}" style="width: 50px; height: 50px; max-width: 100%; max-height: 100%;">
+                            </div>
+                            <div class="product-name" style="flex: 2;">
+                                <p>${producto.nombre}</p>
+                            </div>
+                            <div class="product-quantity" style="flex: 1; display: flex; align-items: center; justify-content: space-between;">
+                                <button class="btn btn-sm btn-secondary decrease-quantity">-</button>
+                                <span>${producto.cantidad}</span>
+                                <button class="btn btn-sm btn-primary increase-quantity">+</button>
+                            </div>
+                        </div>
+                    `;
+                    carProductsList.appendChild(productElement);
+
+                    const decreaseBtn = productElement.querySelector('.decrease-quantity');
+                    const increaseBtn = productElement.querySelector('.increase-quantity');
+                    const quantitySpan = productElement.querySelector('.product-quantity span');
+
+                    decreaseBtn.addEventListener('click', () => {
+                        this.carritoDeCompras.quitarProducto(producto);
+                        renderizarProductosEnCarrito();
+                        actualizarContador();
+                    });
+
+                    increaseBtn.addEventListener('click', () => {
+                        producto.cantidad++;
+                        quantitySpan.textContent = producto.cantidad;
+                        actualizarContador();
+                    });
+                });
+            } else {
+                carProductsList.innerHTML = '<p class="text-center">No hay artículos</p>';
+            }
+        };
+
+        const actualizarContador = () => {
+            const cantidadTotal = this.carritoDeCompras.getCantidadTotal();
+            carritoCounter.textContent = cantidadTotal;
+        };
+
+        renderizarProductosEnCarrito();
+        actualizarContador();
     }
 
-    mostrarProductosEnTienda(){
+    renderizarProductos(productos) {
+        // Obtén el contenedor de productos
+        const contenedor = document.querySelector('.col-lg-9');
 
+        // Limpia el contenedor
+        contenedor.innerHTML = '';
+
+        // Calcula el número de columnas según el tamaño de la pantalla
+        const numColumnas = 4; // Mostrar cuatro imágenes por fila
+
+        // Calcula el número total de páginas
+        const numPaginas = Math.ceil(productos.length / 12);
+
+        // Determina la página actual
+        let paginaActual = 1;
+        const urlParams = new URLSearchParams(window.location.search);
+        const pagina = parseInt(urlParams.get('pagina'));
+        if (!isNaN(pagina) && pagina > 0 && pagina <= numPaginas) {
+            paginaActual = pagina;
+        }
+
+        // Calcula el índice inicial y final de los productos para la página actual
+        const inicio = (paginaActual - 1) * 12;
+        const fin = Math.min(inicio + 12, productos.length);
+
+        // Recorre los productos para la página actual
+        let fila = document.createElement('div');
+        fila.classList.add('row', 'mb-4');
+        for (let i = inicio; i < fin; i++) {
+            const producto = productos[i];
+            const path = producto.getImagen();
+            const nombre = producto.getNombre();
+
+            // Crea la tarjeta de producto
+            const tarjeta = `
+            <div class="col-md-${12 / numColumnas}">
+                <div class="card mb-4 product-wap rounded-0" style="width: 100%; height: 100%;">
+                    <div class="card rounded-0">
+                        <img class="card-img rounded-0 img-fluid" src="${path}" style="max-width: 100%; max-height: 200px; height: auto;">
+                        <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                            <ul class="list-unstyled">
+                                <li><a class="btn btn-success text-white mt-2 image-gallery-add-to-cart" href="#" onclick="addToCartAndRedirect(${JSON.stringify(producto)})"><i class="far fa-eye"></i></a></li>
+                                <li><a class="btn btn-success text-white mt-2 image-gallery-add-to-cart" href="#" onclick="addToCartAndRedirect(${JSON.stringify(producto)})"><i class="fas fa-cart-plus"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <a href="shop-single.html?image=${encodeURIComponent(path)}&name=${encodeURIComponent(nombre)}" class="h3 text-decoration-none">${nombre}</a>
+                        <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                            <li class="pt-2">
+                                <span class="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                                <span class="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                                <span class="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                                <span class="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                                <span class="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        `;
+        
+            fila.innerHTML += tarjeta;
+
+            // Agrega la fila al contenedor cuando se llenen las columnas
+            if ((i + 1 - inicio) % numColumnas === 0 || i === fin - 1) {
+                contenedor.appendChild(fila);
+                fila = document.createElement('div');
+                fila.classList.add('row', 'mb-4');
+            }
+        }
+
+        // Implementa el cambio de página con los botones de navegación
+        const prevPageButton = document.getElementById('prevPage');
+        const nextPageButton = document.getElementById('nextPage');
+
+        // Habilita o deshabilita los botones de navegación según la página actual
+        prevPageButton.disabled = paginaActual === 1;
+        nextPageButton.disabled = paginaActual === numPaginas;
+
+        // Agrega los event listeners a los botones de navegación
+        prevPageButton.addEventListener('click', () => {
+            if (paginaActual > 1) {
+                const nuevaPagina = paginaActual - 1;
+                window.location.href = `shop.html?pagina=${nuevaPagina}`;
+            }
+        });
+
+        nextPageButton.addEventListener('click', () => {
+            if (paginaActual < numPaginas) {
+                const nuevaPagina = paginaActual + 1;
+                window.location.href = `shop.html?pagina=${nuevaPagina}`;
+            }
+        });
     }
 
-    getPaginaActual() {
-        return this.paginaActual;
-    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const tienda = new Tienda();
-    tienda.cargarProductosSiNecesario('productos.csv')
+    tienda.cargarProductosDesdeCSV('productos.csv')
         .then(() => {
+            const productoEjemplo = tienda.getProductoPorNombre('sept_prodEspecializados_03');
+            if (productoEjemplo) {
+                tienda.carritoDeCompras.agregarProducto(productoEjemplo);
+                tienda.renderizarProductosEnCarritoYContador(tienda.getProductos());
+            } else {
+                console.error('Producto no encontrado');
+            }
             tienda.renderizarProductosEnCarritoYContador();
         })
         .catch(error => {
