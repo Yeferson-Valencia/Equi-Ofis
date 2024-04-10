1fd17158aac28edd0518c76502f3855e3e1241ad
Yeferson-Valencia <91446514+Yeferson-Valencia@users.noreply.github.com>
2024-04-10 10:13:09 -0500

    Se modifico la verificacion de la url de la pagina
    en shop para mostrar proudctos en la web

--- Shop.js
+++ Shop.js
@@ -199,7 +199,7 @@ export class Tienda {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    if(window.location.pathname === '/shop.html') {
+    if(window.location.host === 'yeferson-valencia.github.io' && window.location.pathname === '/Equi-Ofis/shop.html') {
 
     const tienda = new Tienda();
 
