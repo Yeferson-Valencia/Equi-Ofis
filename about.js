87ab96a9dbec75ddbc879c937f01e852777a3544
Yeferson-Valencia <pepemanpro.17@gmail.com>
2024-04-22 18:50:31 +0000

    Formulario actualizado con envio a whatsapp

--- about.js
+++ about.js
@@ -20,4 +20,32 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('openModalButton').addEventListener('click', function () {
         myModal.show();
     });
+
+    // Escuchar el evento de envío del formulario
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Evitar el envío del formulario por defecto
+
+        // Obtener los valores del formulario
+        const name = document.getElementById('name').value;
+        const message = document.getElementById('message').value;
+
+        // Construir el mensaje de WhatsApp
+        const whatsappMessage = `Estimado(a) señor(a), mi nombre es ${name}. Me dirijo a usted con el propósito de obtener información sobre: "${message}". Agradezco de antemano su atención y quedo a la espera de su respuesta en mi correo electrónico: ${document.getElementById('email').value}. Muchas gracias por su tiempo.`;
+
+        // Determinar si es dispositivo móvil o de escritorio
+        const esMovil = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+        // Construir el enlace de WhatsApp
+        let url = '';
+        if (esMovil) {
+            // Si es un dispositivo móvil, abrir en la aplicación de WhatsApp
+            url = `https://wa.me/573115288907?text=${encodeURIComponent(whatsappMessage)}`;
+        } else {
+            // Si es un dispositivo de escritorio, abrir en WhatsApp Web
+            url = `https://web.whatsapp.com/send?phone=573115288907&text=${encodeURIComponent(whatsappMessage)}`;
+        }
+
+        // Abrir el enlace en una nueva ventana
+        window.open(url, '_blank');
+    });
 });
